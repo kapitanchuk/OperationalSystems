971fae800987eccbd2fc421e2331b3503b056a94
Dominuskick <sandrkalenikov@gmail.com>
2022-12-03 13:30:58 +0200

    last version

--- Lab9/OneSub.Client/MainWindow.xaml.cs
+++ Lab9/OneSub.Client/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,10 +22,11 @@ namespace OneSub.Client
 {
     public partial class MainWindow : Window
     {
+        static Mutex mutex = new();
+        const string outputFile = "log.txt";
+
         int Port { get; set; }
-        bool currency;
-        bool weather;
-        bool stocks;
+
         public MainWindow(int port)
         {
             Port = port;
@@ -33,59 +37,128 @@ namespace OneSub.Client
 
         private void Listen()
         {
+            using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            var localIP = new IPEndPoint(IPAddress.Loopback, Port);
+
+            udpSocket.Bind(localIP);
+
+            byte[] data = new byte[256];
+
+            EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
             while (true)
             {
-                using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                var localIP = new IPEndPoint(IPAddress.Loopback, Port);
-
-                udpSocket.Bind(localIP);
-
-                byte[] data = new byte[256];
-
-                EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
-
                 var result = udpSocket.ReceiveFrom(data, SocketFlags.None, ref remoteIp);
                 string message = Encoding.UTF8.GetString(data, 0, result);
 
-                if (message.StartsWith("[Currency]") && currency)
-                    Dispatcher.Invoke(() => MsgBox.Text += message);
-                else if (message.StartsWith("[Weather]") && weather)
-                    Dispatcher.Invoke(() => MsgBox.Text += message);
-                else if (message.StartsWith("[Stocks]") && stocks)
-                    Dispatcher.Invoke(() => MsgBox.Text += message);
-                else if (!message.StartsWith('['))
-                    Dispatcher.Invoke(() => MsgBox.Text += message);
+                mutex.WaitOne();
+                File.AppendAllText(outputFile, $"{Port}: {message}\n");
+                mutex.ReleaseMutex();
+
+                if (message.StartsWith("[Currency]"))
+                {
+                    using var reader = new StringReader(message);
+                    reader.ReadLine();
+                    string euro = reader.ReadLine() ?? "0 0";
+                    string usd = reader.ReadLine() ?? "0 0";
+                    string[] euroarr = euro.Split(' ');
+                    string[] usdarr = usd.Split(' ');
+
+                    Dispatcher.Invoke(() => EuroSellBox.Text = euroarr[0]);
+                    Dispatcher.Invoke(() => EuroBuyBox.Text = euroarr[1]);
+                    Dispatcher.Invoke(() => UsdSellBox.Text = usdarr[0]);
+                    Dispatcher.Invoke(() => UsdBuyBox.Text = usdarr[1]);
+                }   
+                else if (message.StartsWith("[Weather]"))
+                {
+                    using var reader = new StringReader(message);
+                    string[] weatherArr = new string[5];
+                    for (int i = 0; i < 5; i++)
+                    {
+                        weatherArr[i] = reader.ReadLine();
+                    }
+                    string[] temperature = weatherArr[1].Split(' ');
+                    string[] feelsLike = weatherArr[2].Split(' ');
+                    string[] pressure = weatherArr[3].Split(' ');
+                    string[] humidity = weatherArr[4].Split(' ');
+
+                    Dispatcher.Invoke(() => TemperatureBox.Text = temperature[1]);
+                    Dispatcher.Invoke(() => FeelsLikeBox.Text = feelsLike[2]);
+                    Dispatcher.Invoke(() => PressureBox.Text = pressure[1]);
+                    Dispatcher.Invoke(() => HumidityBox.Text = humidity[1]);
+                }
+                else if (message.StartsWith("[Stocks]"))
+                {
+                    Debug.WriteLine(message);
+                    using var reader = new StringReader(message);
+                    string[] stocksArr = new string[3];
+                    for (int i = 0; i < 3; i++)
+                    {
+                        stocksArr[i] = reader.ReadLine();
+                    }
+
+                    string[] appleArr = stocksArr[1].Split(' ');
+                    string[] zoomArr = stocksArr[2].Split(' ');
+
+                    Dispatcher.Invoke(() => AppleBox.Text = appleArr[1]);
+                    Dispatcher.Invoke(() => AppleAverageTrades.Text = appleArr[2]);
+                    Dispatcher.Invoke(() => ZoomBox.Text = zoomArr[1]);
+                    Dispatcher.Invoke(() => ZoomAverageTrades.Text = zoomArr[2]);
+                }  
+                //else if (!message.StartsWith('['))
+                  //  Dispatcher.Invoke(() => ServerMessageBox.Text += message);
             }
         }
 
         private void CurrecncyCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            currency = true;
+            SendMessage($"{Port} Currency_On", 7999);
         }
 
         private void WeatherCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            weather = true;
+            SendMessage($"{Port} Weather_On", 7999);
         }
 
         private void StocksCheckBox_Checked(object sender, RoutedEventArgs e)
         {
-            stocks = true;
+            SendMessage($"{Port} Stocks_On", 7999);
         }
 
         private void CurrecncyCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            currency = false;
+         
+            SendMessage($"{Port} Currency_Off", 7999);
         }
 
         private void WeatherCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            weather = false;
+         
+            SendMessage($"{Port} Weather_Off", 7999);
         }
 
         private void StocksCheckBox_Unchecked(object sender, RoutedEventArgs e)
         {
-            stocks = false;
+            SendMessage($"{Port} Stocks_Off", 7999);
+        }
+
+        private static void SendMessage(string message, int port)
+        {
+            var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            mutex.WaitOne();
+            File.AppendAllText("log.txt", message + '\n');
+            mutex.ReleaseMutex();
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            EndPoint remotePoint = new IPEndPoint(IPAddress.Loopback, port);
+            udpSocket.SendTo(data, SocketFlags.None, remotePoint);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SendMessage($"{Port} Currency_Off", 7999);
+            SendMessage($"{Port} Weather_Off", 7999);
+            SendMessage($"{Port} Stocks_Off", 7999);
         }
     }
 }
--- Lab9/OneSub.Server/MainWindow.xaml.cs
+++ Lab9/OneSub.Server/MainWindow.xaml.cs
@@ -1,11 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
 using System.Printing;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -19,30 +22,75 @@ public partial class MainWindow : Window
 {
     private int newPort = 8000;
 
+    class RequiredInfo
+    {
+        public bool currency, weather, stocks;
+    }
+
+    Dictionary<int, RequiredInfo> Infos = new();
+
     Timer currencyTimer = new(1000);
     Timer weatherTimer = new(1000);
-    Timer stocksTimer = new(1000);
+    Timer stocksTimer = new(15000);
 
 
     public MainWindow()
     {
         InitializeComponent();
 
+        if (File.Exists("log.txt"))
+            File.Delete("log.txt");
 
         currencyTimer.Elapsed += async (s, e) => await SendCurrencyInfo();
         weatherTimer.Elapsed += async (s, e) => await SendWeatherInfo();
         stocksTimer.Elapsed += async (s, e) => await SendStocksInfo();
+
+        Task.Run(Listen);
     }
 
     private void Button_Click(object sender, RoutedEventArgs e)
     {
+        Infos.Add(newPort, new RequiredInfo());
         Client.MainWindow newWnd = new(newPort++);
         newWnd.Show();
     }
 
-    private void Button_Click_1(object sender, RoutedEventArgs e)
+    private void Listen()
     {
-        SendMessageToAllClients(CustomTextBox.Text + '\n');
+        using var udpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        var localIP = new IPEndPoint(IPAddress.Loopback, 7999);
+
+        udpSocket.Bind(localIP);
+
+        byte[] data = new byte[256];
+
+        EndPoint remoteIp = new IPEndPoint(IPAddress.Any, 0);
+
+        while (true)
+        {
+            var result = udpSocket.ReceiveFrom(data, SocketFlags.None, ref remoteIp);
+            string message = Encoding.UTF8.GetString(data, 0, result);
+
+            
+
+            string[] dataArr = message.Split();
+            int port = int.Parse(dataArr[0]);
+            string action = dataArr[1];
+
+            if (action is "Currency_On")
+                Infos[port].currency = true;
+            else if (action is "Currency_Off")
+                Infos[port].currency = false;
+            else if (action is "Weather_On")
+                Infos[port].weather = true;
+            else if (action is "Weather_Off")
+                Infos[port].weather = false;
+            else if (action is "Stocks_On")
+                Infos[port].stocks = true;
+            else if (action is "Stocks_Off")
+                Infos[port].stocks = false;
+
+        }
     }
 
     private static void SendMessage(string message, int port)
@@ -58,21 +106,13 @@ public partial class MainWindow : Window
         using HttpClient httpClient = new HttpClient();
         var response = await httpClient.GetAsync("https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5");
 
-        Debug.WriteLine("Here");
-
         using (var responseStreamReader = new StreamReader(response.Content.ReadAsStream()))
         {
             var responseString = responseStreamReader.ReadToEnd();
             JArray currencyInfo = JArray.Parse(responseString);
-            try
-            {
-                string outString = string.Join("\n", currencyInfo.Select(curr => curr["ccy"]?.ToString() + " - " + GetDelta(double.Parse(curr["buy"]?.ToString()), 0.2) + " ; " + GetDelta(double.Parse(curr["sale"]?.ToString()),0.2)));
-                SendMessageToAllClients("[Currency]\n" + outString + "\n" ?? "[Currency] Can`t get info about currencies\n");
-            }
-            catch (Exception)
-            {
-                SendMessageToAllClients("[Currency] Can`t get info about currencies\n");
-            }
+
+            string outString = string.Join("\n", currencyInfo.Select(curr => GetDelta(double.Parse(curr["buy"]?.ToString() ?? "0", CultureInfo.InvariantCulture), 0.2).ToString() + " " + GetDelta(double.Parse(curr["sale"]?.ToString() ?? "0", CultureInfo.InvariantCulture), 0.2).ToString()));
+            SendMessageToAllClients("[Currency]\n" + outString + "\n", port => Infos[port].currency);
         }
     }
 
@@ -87,8 +127,9 @@ public partial class MainWindow : Window
         {
             var str = reader.ReadToEnd();
             var obj = JObject.Parse(str);
-            string outString = "Tempeture: " + obj?["list"]?.First()["main"]?["temp"] + "\nFeels like: " + obj?["list"]?.First()["main"]?["feels_like"] + "\nPressure: " + obj?["list"]?.First()["main"]?["pressure"] + "\nHumidity: " + obj?["list"]?.First()["main"]?["humidity"];
-            SendMessageToAllClients("[Weather]\n" + outString + "\n" ?? "[Weather] Can`t get info about weather\n");
+            
+            string outString = "Temperature: " + obj?["list"]?.First()["main"]?["temp"] + "\nFeels like: " + obj?["list"]?.First()["main"]?["feels_like"] + "\nPressure: " + obj?["list"]?.First()["main"]?["pressure"] + "\nHumidity: " + obj?["list"]?.First()["main"]?["humidity"];
+            SendMessageToAllClients("[Weather]\n" + outString + "\n" ?? "[Weather] Can`t get info about weather\n", port => Infos[port].weather);
         }
     }
 
@@ -103,25 +144,31 @@ public partial class MainWindow : Window
             var obj = JObject.Parse(str);
             try
             {
-                double stockAppl = GetDelta(double.Parse(obj?["AAPL"]?["values"]?.First()["open"]?.ToString()), 1);
-                int volumeAppl = (int)GetDelta(double.Parse(obj?["AAPL"]?["values"]?.First()["volume"]?.ToString()), 5);
-                double stockZm = GetDelta(double.Parse(obj?["ZM"]?["values"]?.First()["open"]?.ToString()), 1);
-                int volumeZm = (int)GetDelta(double.Parse(obj?["ZM"]?["values"]?.First()["volume"]?.ToString()), 5);
-                string outString = "Apple: " + stockAppl.ToString() + "$, traded in one minute:" + volumeAppl.ToString() + "\n"
-                    + "Zoom:  " + stockZm.ToString() + "$, traded in one minute:" + volumeZm.ToString() + "\n";
-                SendMessageToAllClients("[Stocks]\n" + outString + "\n" ?? "[Stocks] Can`t get info about stocks\n");
+                double stockAppl = GetDelta(double.Parse(obj?["AAPL"]?["values"]?.First()["open"]?.ToString() ?? "0", CultureInfo.InvariantCulture), 1);
+
+                int volumeAppl = (int)GetDelta(double.Parse(obj?["AAPL"]?["values"]?.First()["volume"]?.ToString() ?? "0", CultureInfo.InvariantCulture), 5);
+
+                double stockZm = GetDelta(double.Parse(obj?["ZM"]?["values"]?.First()["open"]?.ToString() ?? "0", CultureInfo.InvariantCulture), 1);
+
+                int volumeZm = (int)GetDelta(double.Parse(obj?["ZM"]?["values"]?.First()["volume"]?.ToString() ?? "0"), 5);
+
+                string outString = "Apple: " + stockAppl.ToString() + "$ " + volumeAppl.ToString() + "\n"
+                    + "Zoom: " + stockZm.ToString() + "$ " + volumeZm.ToString() + "\n";
+
+                SendMessageToAllClients("[Stocks]\n" + outString + "\n" ?? "[Stocks] Can`t get info about stocks\n", port => Infos[port].stocks);
             }
             catch (Exception)
             {
-                SendMessageToAllClients("[Stocks] Can`t get info about stocks\n");
+                SendMessageToAllClients("[Stocks] Can`t get info about stocks\n", port => Infos[port].stocks);
             }
         }
     }
 
-    private void SendMessageToAllClients(string message)
+    private void SendMessageToAllClients(string message, Predicate<int> pred)
     {
         for (int port = 8000; port < newPort; port++)
-            SendMessage(message, port);
+            if(pred(port))
+                SendMessage(message, port);
     }
 
     private void Button_Click_2(object sender, RoutedEventArgs e)
@@ -140,15 +187,15 @@ public partial class MainWindow : Window
 
     private static int GetFromString(string str) => str switch
     {
-        "Щосекунди" => 1000,
-        "Щохвилини" => 60000,
-        "Щогодини" => 3600000,
-        _ => throw new NotImplementedException()
+        "Every second" => 1000,
+        "Every 15 seconds" => 15000,
+        "Every 30 seconds" => 30000
+        
     };
 
-    private void SelectionChanged1(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    private void SelectionChanged1(object sender, SelectionChangedEventArgs e)
     {
-        currencyTimer.Interval = GetFromString(CurrencyComboBox.SelectedValue as string);
+        currencyTimer.Interval = GetFromString(CurrencyComboBox.SelectedValue as string ?? " ");
     }
 
     private static double GetDelta(double num, double percent)
